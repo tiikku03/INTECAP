3db834cfefe7f509dfc13e60d4209cbb28e8a504
Victor Emanuel <victorticu2003@gmail.com>
2025-06-07 23:06:48 -0600

    started working with the first project of javaScript

---
+++ Junio/Clase 06-06-2025/Ejercicio/P2 Camas y Salas.js
@@ -0,0 +1,14 @@
+const addCart = document.getElementById("add-cart")
+const shoppingCart = document.getElementById("shoppingCart")
+
+
+function loadANewPage(){
+    window.location.href = "carrito.html";
+}
+
+function addToCart(){
+
+}
+
+shoppingCart.addEventListener("click", loadANewPage)
+
