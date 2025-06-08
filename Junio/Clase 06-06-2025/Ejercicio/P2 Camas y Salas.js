d8770be9f6366d49f6c8303ae8a853d4e19e2dd6
Victor Emanuel <victorticu2003@gmail.com>
2025-06-08 00:10:34 -0600

    continued working wirh the javaSript

--- Junio/Clase 06-06-2025/Ejercicio/P2 Camas y Salas.js
+++ Junio/Clase 06-06-2025/Ejercicio/P2 Camas y Salas.js
@@ -1,14 +1,46 @@
 const addCart = document.getElementById("add-cart")
 const shoppingCart = document.getElementById("shoppingCart")
+const cart = []
 
 
-function loadANewPage(){
-    window.location.href = "carrito.html";
+
+
+function findingTheElement(nodeList, itemToBeFound){
+    let foundItem ="";
+    for (let elementId of nodeList){
+        console.log(elementId)
+        /* if (itemToBeFound === elementId){
+            console.log(elementId)
+        } */
+    }
+    return;
 }
+
 
 function addToCart(){
+    let item = {
+        productName: null,
+        producPrice: null,
+        imageAdress: null
+    }
+    const parentOfCart = addCart.parentElement;
+    const childrenId = parentOfCart.id;
+    const childrenElements = childrenId.childNodes
+    console.log(childrenId)
+   
+    item.producPrice = findingTheElement(childrenElements,"product-price")
+    item.productName = findingTheElement(childrenElements,"product-name")
+    item.imageAdress = findingTheElement(childrenElements,"product-image")
+    
+    console.log(findingTheElement(childrenElements,"product-image"))
+    return item;
 
 }
 
-shoppingCart.addEventListener("click", loadANewPage)
+function showAmmountOfItemsInCart() {
+    return;
+}
 
+
+shoppingCart.addEventListener("click", ()=> window.location.href = "carrito.html")
+console.log(addCart.addEventListener("click",addToCart))
