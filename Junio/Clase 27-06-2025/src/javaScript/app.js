337884dee22e3a136896134185cee867e2df0bdf
Victor Emanuel <victorticu2003@gmail.com>
2025-06-27 22:20:52 -0600

    Empece a trabajar en el index y la pagina de los productos, tambien integre el scss para la pagina

---
+++ Junio/Clase 27-06-2025/src/javaScript/app.js
@@ -0,0 +1,98 @@
+const saborVelozMenus = [
+  {
+    nameMenu: "Cheeseburger Clásica",
+    price: 3.90,
+    imageSrc: "../images/menu1.png", // Ruta de ejemplo para la imagen
+    amount: 0
+  },
+  {
+    nameMenu: "Hamburguesa Doble Queso",
+    price: 4.90,
+    imageSrc: "../images/menu2.png",
+    amount: 0
+  },
+  {
+    nameMenu: "Hamburguesa Champiñones Suizos",
+    price: 4.90,
+    imageSrc: "../images/menu3.png",
+    amount: 0
+  },
+  {
+    nameMenu: "Hamburguesa Jalapeño Picante",
+    price: 3.90,
+    imageSrc: "../images/menu4.png",
+    amount: 0
+  },
+  {
+    nameMenu: "Burrito El Dorado",
+    price: 6.50,
+    imageSrc: "../images/menu5.png",
+    amount: 0
+  },
+  {
+    nameMenu: "Burrito Baja Blast",
+    price: 6.90,
+    imageSrc: "../images/menu6.png",
+    amount: 0
+  },
+  {
+    nameMenu: "Burrito Vegano Picante",
+    price: 5.90,
+    imageSrc: "../images/menu7.png",
+    amount: 0
+  }
+];
+
+const saborVelozBebidas = [
+  {
+    drinkName: "Jugo de Naranja Natural",
+    price: 3.50,
+    imageSrc: "../images/bebida1.webp",
+    amount: 0
+  },
+  {
+    drinkName: "Limonada Refrescante",
+    price: 3.00,
+    imageSrc: "../images/bebida2.webp",
+    amount: 0
+  },
+  {
+    drinkName: "Agua de Jamaica",
+    price: 3.25,
+    imageSrc: "../images/bebida3.webp",
+    amount: 0
+  },
+  {
+    drinkName: "Jugo de Frutos Rojos",
+    price: 3.75,
+    imageSrc: "../images/bebida4.webp",
+    amount: 0
+  },
+  {
+    drinkName: "Agua de Pepino y Menta",
+    price: 3.00,
+    imageSrc: "../images/bebida5.png",
+    amount: 0
+  }
+]
+
+const postres = [
+  {
+    dessertName: "Pastel de Chocolate",
+    price: 4.99,
+    imageSrc: "images/chocolate-cake.jpg",
+    amount: 10
+  },
+  {
+    dessertName: "Helado de Vainilla",
+    price: 2.50,
+    imageSrc: "images/vanilla-ice-cream.jpg",
+    amount: 25
+  },
+  {
+    dessertName: "Tarta de Manzana",
+    price: 3.75,
+    imageSrc: "images/apple-pie.jpg",
+    amount: 15
+  }
+];
