6f652258e64849aece0fc3d21a51f1dff1c44aa0
Victor Emanuel <victorticu2003@gmail.com>
2025-06-30 21:40:28 -0600

    Termine con el proycto tarea de tienda de comida rapida

--- Junio/Clase 27-06-2025/src/javaScript/app.js
+++ Junio/Clase 27-06-2025/src/javaScript/app.js
@@ -1,98 +1,208 @@
 const saborVelozMenus = [
   {
     nameMenu: "Cheeseburger Clásica",
-    price: 3.90,
+    price: 39,
     imageSrc: "../images/menu1.png", // Ruta de ejemplo para la imagen
-    amount: 0
   },
   {
     nameMenu: "Hamburguesa Doble Queso",
-    price: 4.90,
+    price: 49,
     imageSrc: "../images/menu2.png",
-    amount: 0
   },
   {
     nameMenu: "Hamburguesa Champiñones Suizos",
-    price: 4.90,
+    price: 49,
     imageSrc: "../images/menu3.png",
-    amount: 0
   },
   {
     nameMenu: "Hamburguesa Jalapeño Picante",
-    price: 3.90,
+    price: 39,
     imageSrc: "../images/menu4.png",
-    amount: 0
   },
   {
     nameMenu: "Burrito El Dorado",
-    price: 6.50,
+    price: 39,
     imageSrc: "../images/menu5.png",
-    amount: 0
   },
   {
     nameMenu: "Burrito Baja Blast",
-    price: 6.90,
+    price: 39,
     imageSrc: "../images/menu6.png",
-    amount: 0
   },
   {
     nameMenu: "Burrito Vegano Picante",
-    price: 5.90,
+    price: 50,
     imageSrc: "../images/menu7.png",
-    amount: 0
-  }
-];
-
-const saborVelozBebidas = [
+  },
   {
-    drinkName: "Jugo de Naranja Natural",
-    price: 3.50,
+    nameMenu: "Jugo de Naranja Natural",
+    price: 15,
     imageSrc: "../images/bebida1.webp",
-    amount: 0
   },
   {
-    drinkName: "Limonada Refrescante",
-    price: 3.00,
+    nameMenu: "Limonada Refrescante",
+    price: 10,
     imageSrc: "../images/bebida2.webp",
-    amount: 0
   },
   {
-    drinkName: "Agua de Jamaica",
-    price: 3.25,
+    nameMenu: "Agua de Jamaica",
+    price: 20,
     imageSrc: "../images/bebida3.webp",
-    amount: 0
   },
   {
-    drinkName: "Jugo de Frutos Rojos",
-    price: 3.75,
+    nameMenu: "Jugo de Frutos Rojos",
+    price: 15,
     imageSrc: "../images/bebida4.webp",
-    amount: 0
   },
   {
-    drinkName: "Agua de Pepino y Menta",
-    price: 3.00,
+    nameMenu: "Agua de Pepino y Menta",
+    price: 10,
     imageSrc: "../images/bebida5.png",
-    amount: 0
-  }
-]
+  },
 
-const postres = [
   {
-    dessertName: "Pastel de Chocolate",
-    price: 4.99,
+    nameMenu: "Pastel de Chocolate",
+    price: 30,
     imageSrc: "images/chocolate-cake.jpg",
-    amount: 10
   },
   {
-    dessertName: "Helado de Vainilla",
-    price: 2.50,
+    nameMenu: "Helado de Vainilla",
+    price: 25,
     imageSrc: "images/vanilla-ice-cream.jpg",
-    amount: 25
   },
   {
-    dessertName: "Tarta de Manzana",
-    price: 3.75,
+    nameMenu: "Helado de Frutas",
+    price: 20,
     imageSrc: "images/apple-pie.jpg",
-    amount: 15
-  }
+  },
 ];
+
+const buttonMenu = document.getElementsByClassName("comprarMenu");
+
+function managingUserPurchase(object, amountRequested) {
+  if (amountRequested !== "") {
+    (object.amountUserWants = parseInt(amountRequested)),
+      (object.bill = amountRequested * object.price);
+    return object.bill;
+  }
+}
+
+function managingUserPurchaseDiscount(object, amountRequested) {
+  let discount = 0;
+  if (amountRequested !== "") {
+    object.amountUserWants = parseInt(amountRequested);
+    discount = Number((amountRequested * object.price * 0.05).toFixed(2));
+    object.bill = Number((amountRequested * object.price - discount).toFixed(2));
+  }
+  return discount;
+}
+
+function postrePromocional(object, amountRequested, container) {
+    if (amountRequested !== "") {
+      object.amountUserWants = parseInt(amountRequested) + 1;
+      object.bill = (object.amountUserWants * object.price);
+      container.innerHTML = `Ha comprado 3 o mas postres, Ha recibido una mas gratis`;
+    }
+  
+}
+
+// para los botones de menu
+for (let i = 0; i < buttonMenu.length; i++) {
+  buttonMenu[i].addEventListener("click", (event) => {
+    let menuCollection = event.currentTarget.parentNode.children;
+    let currentProductName = menuCollection[1];
+    let purrchaseAmount = menuCollection[4].value;
+    let descuentoContainer = menuCollection[5];
+
+    for (product of saborVelozMenus) {
+      if (product.nameMenu === currentProductName.innerText) {
+        if (
+          currentProductName.parentNode.parentNode.className === "menus" &&
+          purrchaseAmount >= 3
+        ) {
+          let discount = managingUserPurchaseDiscount(product, purrchaseAmount);
+          descuentoContainer.innerHTML = `El descuento es de: ${discount} 
+          <br> Costo de los menus: Q.${product.bill}`;
+        } else if (currentProductName.parentNode.parentNode.className === "menus" && purrchaseAmount < 3
+        ) {
+          managingUserPurchase(product, purrchaseAmount);
+          descuentoContainer.innerHTML = `No hay descuento, debes comprar 3 o mas <br> Costo de los menus: Q.${product.bill}`;
+        } else if (purrchaseAmount >= 3 && currentProductName.parentNode.parentNode.className === "postres") {
+          postrePromocional(product,purrchaseAmount, descuentoContainer);
+        } else {
+          managingUserPurchase(product, purrchaseAmount);
+          descuentoContainer.innerHTML = `<br> Costo de los menus: Q.${product.bill}`;
+        }
+      }
+    }
+  });
+}
+
+let billElement = document.getElementById("bill");
+
+function gettingTotalBill(Array) {
+  let productsInTheCart = Array.filter((element) => {
+    if (element.hasOwnProperty("amountUserWants")) {
+      return element;
+    }
+  });
+
+  let totalBill = productsInTheCart.reduce((accu, product) => {
+    return product.bill + accu;
+  }, 0);
+
+  let htmlCode = productsInTheCart.map((element) => {
+    return `
+      <tr>
+      <td>${element.amountUserWants}</td>
+      <td>${element.nameMenu}</td>
+      <td class="productAmount">Q.${element.bill}</td>
+      </tr>
+      `;
+  });
+
+  billElement.classList.add("bill");
+  billElement.innerHTML = `
+                         <tr>
+                         <td><strong>Factura</strong></td>
+                         </tr>
+                         ${htmlCode.join("")}
+                         <tr>    
+                         <td></td>
+                         <td><strong>Total</strong></td>
+                         <td class="productAmount"><strong>Q.${totalBill}</strong></td>
+                         </tr>`;
+}
+
+for (let i = 0; i < buttonMenu.length; i++) {
+  buttonMenu[i].addEventListener("click", () =>
+    gettingTotalBill(saborVelozMenus)
+  );
+}
+
+
+// Desarrollando el formulario de compra
+
+
+let formContainer = document.getElementById("endMessage")
+let form = document.getElementById("billingForm")
+
+
+
+ form.addEventListener("submit",(event)=>{
+  event.preventDefault();
+  const datosFormulario = {};
+  for (let i = 0; i < event.target.elements.length; i++) { // Use event.target.elements
+      const elemento = event.target.elements[i];
+      if (!elemento.name || elemento.type === 'submit') {
+        continue;
+      }else{
+        datosFormulario[elemento.name] = elemento.value;
+      }
+    }
+  formContainer.innerHTML = `
+  Gracias <strong>${datosFormulario.userName}</strong> por realizar la compre,
+  los articulos se enviaran a <strong>${datosFormulario.address}</strong>, nos comunicaremos a
+  su numero de celular proporcionado; <strong>${datosFormulario.phoneNumber}</strong>`
+}) 
+
