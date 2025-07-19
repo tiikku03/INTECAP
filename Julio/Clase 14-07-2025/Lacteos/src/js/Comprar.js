924e9216160e27b2373b8229e487c48b341f336c
Victor Emanuel <victorticu2003@gmail.com>
2025-07-18 23:28:01 -0600

    Pagina de lacteos Terminada

--- Julio/Clase 14-07-2025/Lacteos/src/js/Comprar.js
+++ Julio/Clase 14-07-2025/Lacteos/src/js/Comprar.js
@@ -1,18 +1,34 @@
-const params = new URLSearchParams(window.location.search);
+// Ensure this script runs after the DOM is loaded.
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
 
-const producto = params.get("Producto");
-const precio = parseInt(params.get("Precio"));
-const cantidad = params.get("cantidad");
+    const producto = params.get("Producto");
+    const precio = parseInt(params.get("Precio"));
+    const cantidad = parseInt(params.get("cantidad"));
 
-document.getElementById('nombreProducto').textContent = "Producto: " + producto ;
-document.getElementById('precioProducto').textContent = "Precio " + precio;
-document.getElementById("cantidadProducto").textContent = "Cantidad: " + cantidad;
-// alt 96
-``
-const nombreComprador = document.getElementById('customerName');
-const direccionComprador = document.getElementById('Address');
-const phoneComprador = document.getElementById('Phone');
-const correoComprador = document.getElementById('Email');
 
-let total = "Total: " + precio  * cantidad;
-console.log(total);
+    document.getElementById('nombreProducto').textContent = "Producto: " + producto;
+    document.getElementById('precioProducto').textContent = "Precio: " + precio; 
+    document.getElementById("cantidadProducto").textContent = "Cantidad: " + cantidad;
+
+    let totalCalculado = precio * cantidad;
+    document.getElementById("totalproducto").textContent = "Total: " + totalCalculado;
+
+    const form = document.querySelector('form');
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault(); 
+
+        const nombreComprador = document.getElementById('customerName').value;
+        const direccionComprador = document.getElementById('Address').value;
+        const phoneComprador = document.getElementById('Phone').value;
+        const correoComprador = document.getElementById('Email').value;
+
+        let formContainer = document.getElementById("endMessage");
+        formContainer.innerHTML = `
+            Gracias <strong>${nombreComprador}</strong> por realizar la compra,
+            los artículos se enviarán a <strong>${direccionComprador}</strong>. Nos comunicaremos a
+            su número de celular proporcionado: <strong>${phoneComprador}</strong> o a su correo electrónico: <strong>${correoComprador}</strong>.`;
+        form.reset(); 
+    });
+});
