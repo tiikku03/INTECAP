7612c4f3b30f11e65633625710d3051f8c101e6e
Victor Emanuel <victorticu2003@gmail.com>
2025-08-01 20:59:42 -0600

    Carrito de compras terminado

--- Julio/Clase 28-07-2025/Office Depot/src/js/Carrito2.js
+++ Julio/Clase 28-07-2025/Office Depot/src/js/Carrito2.js
@@ -1,10 +1,27 @@
-const arrayLocalStorage = `"[
-        {"producto":"Resma de papel",
-        "descripcion":"Papel multiusos para impresión y copiado.",
-        "precio":75,
-        "imagen_url":"https://www.spanishdict.com/translate/la%20imagen",
-        "cantidad":4}
-]"`
+const arrayLocalStorage = [
+    {
+        "producto": "Resma de papel",
+        "descripcion": "Papel multiusos para impresión y copiado.",
+        "precio": 75,
+        "imagen_url": "https://www.spanishdict.com/translate/la%20imagen",
+        "cantidad": 4
+    }
+];
+
+function updateCartCounter(){
+    const items = JSON.parse(localStorage.getItem("cart")) || []; 
+    const cartLength = items.length;
+    const CartCount = document.querySelector("#cart-count");
+
+    if(CartCount){
+        CartCount.textContent = cartLength;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", updateCartCounter);
+
+ 
+
 
 
 
